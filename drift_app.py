313442706a91e47d7efe7c1a65f5c3b4e1c248ea
kamal15601 <67511752+kamal15601@users.noreply.github.com>
2025-11-20 14:33:19 +0530

    Update drift_app.py

--- drift_app.py
+++ drift_app.py
@@ -926,4 +926,4 @@ if __name__ == '__main__':
     scanner_thread.start()
     
     # Run Flask app
-    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
+    app.run(debug=True, host='0.0.0.0', port=8000, threaded=True)
